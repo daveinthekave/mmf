37ea87b825d61a7bc0ba94ad086257e00a7ac6fb
David Krause <david@krause-net.org>
2020-12-05 17:16:12 +0100

    blub

--- simulated_annealing.m
+++ simulated_annealing.m
@@ -1,17 +1,17 @@
 function [slm_phase_mask] = simulated_annealing(input, target)
 %UNTITLED Summary of this function goes here
-T_start = 20;
+T_start = 200;
 start_input = input;
 % calcs inital mode result
 previous_result = fftshift(fft2(input));
-previous_fidelity = abs(innerproduct(target, previous_result))^2;
-for T=Tstart:-1:0
+previous_fidelity = abs(innerProduct(target, previous_result))^2;
+for T=T_start:-1:0
     current_input = input;
-    random_index = round(rand(1, 2) .* size(input), 0);
+    random_index = round(rand(1, 2) .* size(input), 0) + 1;
     current_input(random_index) = current_input(random_index) * exp(1i*rand() * 2 * pi);
     
     current_result = fftshift(fft2(current_input));
-    current_fidelity = abs(innerproduct(target, current_result))^2;
+    current_fidelity = abs(innerProduct(target, current_result))^2;
     
     if current_fidelity > previous_fidelity
         input = current_input;
