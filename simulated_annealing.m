319c8181686688e74b46e70faf4686840e5c39eb
David Krause <david@krause-net.org>
2020-12-05 17:01:34 +0100

    sim ann implementiert

--- simulated_annealing.m
+++ simulated_annealing.m
@@ -1,7 +1,31 @@
 function [slm_phase_mask] = simulated_annealing(input, target)
 %UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+T_start = 20;
+start_input = input;
+% calcs inital mode result
+previous_result = fftshift(fft2(input));
+previous_fidelity = abs(innerproduct(target, previous_result))^2;
+for T=Tstart:-1:0
+    current_input = input;
+    random_index = round(rand(1, 2) .* size(input), 0);
+    current_input(random_index) = current_input(random_index) * exp(1i*rand() * 2 * pi);
+    
+    current_result = fftshift(fft2(current_input));
+    current_fidelity = abs(innerproduct(target, current_result))^2;
+    
+    if current_fidelity > previous_fidelity
+        input = current_input;
+        previous_fidelity = current_fidelity;
+    else
+        delta_E = previous_fidelity - current_fidelity;
+        P = exp(-delta_E/T);
+        R = rand();
+        if R < P
+            input = current_input;
+            previous_fidelity = current_fidelity;
+        end
+    end
+end
+slm_phase_mask = angle(input);  % - angle(start_input);
 end
 
