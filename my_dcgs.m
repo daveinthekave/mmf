7b46a0ec5c7e19dadb4ca22f3ccce5d03e2d4dd3
Sebastian Heide <sebastian.heide@yahoo.de>
2020-12-16 14:00:12 +0100

    rel_area hinzugefügt und sachen werden genormt

--- my_dcgs.m
+++ my_dcgs.m
@@ -4,15 +4,15 @@ n_it = 200;
 free = size(input);
 signal = size(target);
 
-input_amp = abs(input); % ones(desired_beam_size);
+input_amp = abs(input)./max(max(abs(input)));
 input_phase = 2*pi * rand(free);
 input = input_amp .* exp(1i*input_phase);
 % normalisiere target_amp damit es dcgs schneller konvergiert
-target_amp = abs(target);  % ./max(max(abs(target)));
+target_amp = abs(target)./max(max(abs(target)));
 target_phase = angle(target);
 
-start = free / 2 - signal / 2;
-stop = free / 2 + signal / 2 -1;
+start = round(free / 2 - signal / 2);
+stop = round(free / 2 + signal / 2 - 1);
 temp = zeros(free);
 temp(start:stop, start:stop) = ones(signal);
 mask = temp >= 1;
@@ -27,7 +27,6 @@ for i=0:n_it
     fft_phase(mask) = target_phase;
     
     new_input = ifft2(ifftshift(fft_amp .* exp(1i*fft_phase)));
-    test = ifft2(fft_amp .* exp(1i*fft_phase));
     
     input = input_amp .* exp(1i*angle(new_input));
 end
@@ -41,18 +40,5 @@ stop_phase = phase_values(end) + phase_step/2;
 phase_edges = start_phase:phase_step:stop_phase;
 
 slm_mask = discretize(desired_phase, phase_edges, phase_values);
-test_good = fftshift(fft2(input_amp .* exp(1i*desired_phase)));
-test_bad = fftshift(fft2(input_amp .* exp(1i*slm_mask)));
-diff = desired_phase - slm_mask;
-desired_phase(10, 10) = desired_phase(10, 10) * 1.001;
-test_c = fftshift(fft2(input_amp .* exp(1i*desired_phase)));
-
-% figure('name', 'diff phase maske');imagesc(diff);
-% figure('name', 'good abs');imagesc(abs(test_good) .* mask);
-% figure('name', 'good angle');imagesc(angle(test_good) .* mask);
-% figure('name', 'c abs');imagesc(abs(test_c) .* mask);
-% figure('name', 'c angle');imagesc(angle(test_c) .* mask);
-% figure('name', 'bad abs');imagesc(abs(test_bad) .* mask);
-% figure('name', 'bad angle');imagesc(angle(test_bad) .* mask);
 end
 
--- shift.m
+++ shift.m
@@ -7,7 +7,9 @@ invers = ifft2(no_shift);
 shifted_invers = ifft2(shifted);
 invers_shifted = ifft2(ifftshift(no_shift));
 double_shifted_invers = ifft2(ifftshift(shifted));
-
+max(max(shifted_invers - invers))
+max(max(invers_shifted - invers))
+max(max(double_shifted_invers - invers))
 figure;imagesc(abs(invers));title('no shift, no shift');
 figure;imagesc(abs(shifted_invers));title('shift, no shift');
 figure;imagesc(abs(invers_shifted));title('no shift, shift');
