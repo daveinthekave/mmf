6977aa94c6f7e111dc43e152fed21fc30bcea8de
David Krause <david@krause-net.org>
2020-12-16 17:59:55 +0100

    temp

--- my_dcgs.m
+++ my_dcgs.m
@@ -1,22 +1,6 @@
 function [slm_mask, mask] = my_dcgs(input, target, bit_res)
 n_it = 60;
 
-free = size(input);
-signal = size(target);
-
-input_amp = abs(input)./max(max(abs(input)));
-input_phase = 2*pi * rand(free);
-input = input_amp .* exp(1i*input_phase);
-% normalisiere target_amp damit es dcgs schneller konvergiert
-target_amp = abs(target)./max(max(abs(target)));
-target_phase = angle(target);
-
-start = round(free / 2 - signal / 2);
-stop = round(free / 2 + signal / 2 - 1);
-temp = zeros(free);
-temp(start(1):stop(1), start(1):stop(1)) = ones(signal);
-mask = temp >= 1;
-
 % discretizes the phase
 phase_values = linspace(-pi, pi, 2^bit_res);
 phase_step = abs(phase_values(1) - phase_values(2));
@@ -24,6 +8,22 @@ phase_step = abs(phase_values(1) - phase_values(2));
 start_phase = phase_values(1) - phase_step/2;
 stop_phase = start_phase + 2^bit_res * phase_step;
 phase_edges = start_phase:phase_step:stop_phase;
+free = size(input);
+signal = size(target);
+
+input_amp = abs(input)./max(max(abs(input)));
+rand_phase = 2*pi * rand(free) - pi;
+input_phase = discretize(rand_phase, phase_edges, phase_values);
+input = input_amp .* exp(1i*input_phase);
+% normalisiere target_amp damit es dcgs schneller konvergiert
+target_amp = abs(target)./max(max(abs(target)));
+target_phase = discretize(angle(target), phase_edges, phase_values);
+
+start = round(free / 2 - signal / 2);
+stop = round(free / 2 + signal / 2 - 1);
+temp = zeros(free);
+temp(start(1):stop(1), start(1):stop(1)) = ones(signal);
+mask = temp >= 1;
 
 for i=1:n_it
     input_fft = fftshift(fft2(input));
