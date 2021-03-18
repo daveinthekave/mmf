c90a531d32d711bed9d78bab91037e2de4b040f4
David Krause <david@krause-net.org>
2021-03-18 16:41:14 +0100

    plot-distance = 1.1

--- plot_relative_area.m
+++ plot_relative_area.m
@@ -21,55 +21,57 @@ N=50;
 j = 1;
 for rel_area=0.02:0.02:0.98
     d_sig = round(d_free * sqrt(rel_area));
-    modes=build_modes(nCore,nCladding,wavelength,coreRadius,d_sig);
-    target=squeeze(modes(mode,:,:));
+    if mod(d_sig, 2) == 0
+        modes=build_modes(nCore,nCladding,wavelength,coreRadius,d_sig);
+        target=squeeze(modes(mode,:,:));
 
-    start = round(d_free/2 - d_sig/2);
-    stop = round(d_free/2 + d_sig/2 - 1);
+        start = round(d_free/2 - d_sig/2);
+        stop = round(d_free/2 + d_sig/2 - 1);
 
-    [X,Y] = meshgrid(1:d_free,1:d_free);
-    area_analysis=false(d_free,d_free);
-    area_analysis((X-d_free/2).^2+(Y-d_free/2).^2 <= (d_sig/2-1)^2)=true;
-    %%
-    target_amp=abs(target)./max(max(abs(target)));
-    target_phase=angle(target);
-    fidelity_target = target_amp .* exp(1i*target_phase);
+        [X,Y] = meshgrid(1:d_free,1:d_free);
+        area_analysis=false(d_free,d_free);
+        area_analysis((X-d_free/2).^2+(Y-d_free/2).^2 <= (d_sig/2-1)^2)=true;
+        %%
+        target_amp=abs(target)./max(max(abs(target)));
+        target_phase=angle(target);
+        fidelity_target = target_amp .* exp(1i*target_phase);
 
-    input_amp=ones(d_free,d_free);
-    input_phase=ones(d_free,d_free);
+        input_amp=ones(d_free,d_free);
+        input_phase=ones(d_free,d_free);
 
-    Input=input_amp.*exp(1i*input_phase);
+        Input=input_amp.*exp(1i*input_phase);
 
-    %% Popagation parameter
-    dx=8e-6;dy=dx;      % pixel size SLM [m]
-    lambda=532e-9;      % wavelength [m]
-    dist=0.5;           % propagation distance [m]
+        %% Popagation parameter
+        dx=8e-6;dy=dx;      % pixel size SLM [m]
+        lambda=532e-9;      % wavelength [m]
+        dist=0.5;           % propagation distance [m]
 
-    for i=1:N
-        target_plane=prop(Input,dx,dy,lambda,dist);
+        for i=1:N
+            target_plane=prop(Input,dx,dy,lambda,dist);
 
-        target_plane_amp=abs(target_plane);
-        target_plane_phase=angle(target_plane);
+            target_plane_amp=abs(target_plane);
+            target_plane_phase=angle(target_plane);
 
-        target_plane_amp(start:stop, start:stop) = target_amp;
-        target_plane_phase(start:stop, start:stop) = target_phase;
+            target_plane_amp(start:stop, start:stop) = target_amp;
+            target_plane_phase(start:stop, start:stop) = target_phase;
 
-        backprop_field=target_plane_amp.*exp(1i*target_plane_phase);
+            backprop_field=target_plane_amp.*exp(1i*target_plane_phase);
 
-        % correct phase
-        PhaseCorrected=angle(prop(backprop_field,dx,dy,lambda,-dist));
-        PhaseCorrected(PhaseCorrected<0) = PhaseCorrected(PhaseCorrected<0) + 2*pi;
+            % correct phase
+            PhaseCorrected=angle(prop(backprop_field,dx,dy,lambda,-dist));
+            PhaseCorrected(PhaseCorrected<0) = PhaseCorrected(PhaseCorrected<0) + 2*pi;
 
-        disc_phase = our_disc(PhaseCorrected, bit_resolution);
+            disc_phase = our_disc(PhaseCorrected, bit_resolution);
 
-        Input=input_amp.*exp(1i*disc_phase);
+            Input=input_amp.*exp(1i*disc_phase);
+        end
+
+        modulated = prop(Input,dx,dy,lambda,dist);
+        fidelity_vals(j) = our_calc_fidelity(fidelity_target, modulated, area_analysis);
+        %ssim_vals(round(rel_area*100)) = complex_ssim(fidelity_target, modulated, area_analysis);
+        rel_areas(j) = rel_area;
+        j = j + 1;
     end
-    
-    modulated = prop(Input,dx,dy,lambda,dist);
-    fidelity_vals(j) = our_calc_fidelity(fidelity_target, modulated, area_analysis);
-    %ssim_vals(round(rel_area*100)) = complex_ssim(fidelity_target, modulated, area_analysis);
-    rel_areas(j) = rel_area;
-    j = j + 1;
 end
 save(fullfile(root, strcat(num2str(br), '-bit-fidelity_vals')), 'fidelity_vals');
 % save(strcat(root, num2str(d_free), 'area-ssim'), 'ssim_vals');
